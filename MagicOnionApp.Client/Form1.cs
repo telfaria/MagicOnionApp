dd1f2e173f1a2610b418742726ba9cd35e101cfc
Elfaria <elfaria@ctknet.ne.jp>
2023-10-14 00:40:59 +0900

    StreamingHubを実装してみたけど動かない

--- MagicOnionApp.Client/Form1.cs
+++ MagicOnionApp.Client/Form1.cs
@@ -4,6 +4,8 @@ using MessagePack;
 using System.Threading.Channels;
 using Grpc.Core;
 using Grpc.Net.Client;
+using MagicOnion.Server.Hubs;
+using MagicOnionApp.Hubs;
 using MagicOnionApp.Shared;
 
 namespace MagicOnionApp.Client
@@ -12,6 +14,10 @@ namespace MagicOnionApp.Client
     {
         private GrpcChannel channel;
         private IMagicOnionAppService client;
+        private CancellationTokenSource shutdowncancelletion = new CancellationTokenSource();
+        private IGroupHub streamingclient;
+        private IGroupHubReceiver streamingclientreceiver;
+
         public Form1()
         {
             InitializeComponent();
@@ -22,23 +28,32 @@ namespace MagicOnionApp.Client
             connect();
         }
 
-        private void connect()
+        private async void connect()
         {
             channel = GrpcChannel.ForAddress("http://localhost:5000");
             client = MagicOnionClient.Create<IMagicOnionAppService>(channel);
-
+            streamingclient =
+                await StreamingHubClient.ConnectAsync<IGroupHub, IGroupHubReceiver>(channel, streamingclientreceiver,
+                    cancellationToken: shutdowncancelletion.Token);
+            
             txtResult.AppendText("Connect: http://localhost:5000 \r\n");
+            //await streamingclient.JoinAsync("RoomC", "User1");
+            
 
         }
 
         private async void btnQuery_Click(object sender, EventArgs e)
         {
-            int x = new Random().Next();
-            int y = new Random().Next();
+            int x = new Random().Next(1,100);
+            int y = new Random().Next(1,100);
 
             var result = await client.SumAsync(x, y);
 
             txtResult.AppendText($"{x} + {y} = {result.ToString()}\r\n");
         }
+
+
+
+
     }
 }
---
+++ MagicOnionApp/MagicOnion.Hubs.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using MagicOnion;
+using MagicOnion.Server;
+using MagicOnion.Server.Hubs;
+using MessagePack;
+
+namespace MagicOnionApp.Hubs
+{
+
+    public interface IGroupHub : IStreamingHub<IGroupHub, IGroupHubReceiver>
+    {
+        Task JoinAsync(string roomname, string name);
+
+        Task LeaveAsync();
+
+        Task SendMessageAsync(string username, string message);
+    }
+
+    public interface IGroupHubReceiver
+    {
+        void OnJoin(string name);
+
+        void OnLeave(string name);
+
+        void OnSendMessage(string name, string message);
+    }
+
+
+    public class GroupHubReceiver : StreamingHubBase<IGroupHub,IGroupHubReceiver> , IGroupHub
+    {
+        private IGroup group;
+        private string username;
+
+        public async Task JoinAsync(string room, string name)
+        {
+            this.username = name;
+            group = await Group.AddAsync(room);
+            Broadcast(group).OnJoin(name);
+        }
+
+        public async Task LeaveAsync()
+        {
+            await group.RemoveAsync(this.Context);
+            Broadcast(group).OnLeave(username);
+        }
+
+        public async Task SendMessageAsync(string name, string message)
+        {
+            Broadcast(group).OnSendMessage(name, message);
+            await Task.CompletedTask;
+        }
+
+    }
+
+    public class Grouphub : IGroupHubReceiver
+    {
+        private string roomname = "RoomC";
+        private string username = "user1";
+
+        public async void OnJoin(string name)
+        {
+            //return $"Connected {name}";
+        }
+
+        public async void OnLeave(string name)
+        {
+            //return $"Disconnected {name}";
+        }
+
+        public async void OnSendMessage(string name, string message)
+        {
+            //return $"{name} : {message}";
+        }
+    }
+
+}
